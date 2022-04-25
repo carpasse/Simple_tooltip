7583cbfcea75aff834219b5ef9244b99fb1a6f0a
Carlos Serrano <carlos.pastor@mailonline.co.uk>
2022-04-25 17:23:00 +0200

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,34 +1,55 @@
 import "./styles.css";
 
+const TOOLTIP_TIP_HEIGHT = 15;
+
 const initTooltips = () => {
   const tooltipElements = Array.from(
     document.querySelectorAll("[data-tooltip]")
   );
-  const tooltipContainer = document.createElement("div");
+  const tooltip = document.createElement("div");
+  const tooltipBox = document.createElement("div");
   const tooltipMouseOverHandler = (event) => {
     // TODO: POSITION TOOLTIP TIP to follow the caret
   };
 
-  const tooltipMouseEneterHandler = (event) => {
-    const element = event.target;
-    const text = element.dataSet.tooltip;
+  const tooltipMouseEnterHandler = (event) => {
+    const target = event.target;
+    const text = target.dataset.tooltip;
 
     if (text) {
+      tooltip.innerHTML = text;
       // TODO: POSITION TOOLTIP ON TOP RIGHT IF THERE IS SPACE ELSE POSITION ON TOP BOTTOM
-      const rect = element.getBoundingClientRect();
-      tooltipContainer.innerHTML = text;
+      const targetRect = target.getBoundingClientRect();
+      const tooltipRect = tooltip.getBoundingClientRect();
+
+      if (targetRect.top > tooltipRect.height + TOOLTIP_TIP_HEIGHT) {
+        const tooltipTop =
+          targetRect.top - tooltipRect.height - TOOLTIP_TIP_HEIGHT;
+
+        tooltip.style.top = `${tooltipTop}px`;
+      } else {
+        const tooltipTop =
+          targetRect.top + tooltipRect.height + TOOLTIP_TIP_HEIGHT;
+
+        tooltip.style.top = `${tooltipTop}px`;
+      }
+
+      tooltip.classList.add("active");
     }
   };
 
   const tooltipMouseLeaveHandler = () => {
-    tooltipContainer.innerHTML = "";
+    tooltip.innerHTML = "";
+    tooltip.classList.remove("active");
   };
 
-  tooltipContainer.classList.add("tooltip");
-  document.body.appendChild(tooltipContainer);
+  tooltip.classList.add("tooltip");
+  tooltipBox.classList.add("box");
+  tooltip.appendChild(tooltipBox);
+  document.body.appendChild(tooltip);
 
   for (const element of tooltipElements) {
-    element.addEventListener("mouseenter", tooltipMouseEneterHandler);
+    element.addEventListener("mouseenter", tooltipMouseEnterHandler);
     element.addEventListener("mouseleave", tooltipMouseLeaveHandler);
     element.addEventListener("mouseover", tooltipMouseOverHandler);
   }
@@ -36,7 +57,7 @@ const initTooltips = () => {
   // Return a cleanup function
   return () => {
     for (const element of tooltipElements) {
-      element.removeEventListener("mouseenter", tooltipMouseEneterHandler);
+      element.removeEventListener("mouseenter", tooltipMouseEnterHandler);
       element.removeEventListener("mouseleave", tooltipMouseLeaveHandler);
       element.removeEventListener("mouseover", tooltipMouseOverHandler);
     }
